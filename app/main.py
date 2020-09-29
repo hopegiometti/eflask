5648634c9e865bb5f6243ad8a1ebafa2ea0cf6d5
Hope <hopegiometti@gmail.com>
2020-09-29 11:05:06 -0600

    added spells json

--- app/main.py
+++ app/main.py
@@ -1,7 +1,37 @@
 from flask import Flask 
+from flask import jsonify 
+import json
+from flask_cors import CORS
+# from flask_sqlalchemy import SQLAlchemy
   
 app = Flask(__name__) 
   
-@app.route("/") 
-def home_view(): 
-    return "<h1>Welcome to Geeks for Geeks</h1>"
+# @app.route("/") 
+# def home_view(): 
+#     return "<h1>Welcome to Geeks for Geeks</h1>"
+
+spells = json.load(open("files/spells.json"))
+
+app=Flask(__name__)
+CORS(app)
+
+@app.route('/')
+
+def home():
+    allspellnames = []
+
+    for spell in spells:
+        allspellnames.append(spell)
+    
+    return jsonify(allspellnames)
+
+# def home():
+#     return a
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+#to run a flask app
+#flask run 
